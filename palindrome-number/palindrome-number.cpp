5f83e6f1802d622ed2d4d433ca8f8859b11572f2
spol-29 <polineni.sriram9@gmail.com>
2023-03-01 19:53:46 -0600

    update readme and complete palindrome problem

--- palindrome-number/palindrome-number.cpp
+++ palindrome-number/palindrome-number.cpp
@@ -1,22 +1,18 @@
-#include<bits/stdc++.h>
-using namespace std;
-int main(){
-    int x;
-    cin >> x;
-    int n = x;
-    int b = 0;
-    while(x != 0){
-        b *= (x%10);
-        b *= 10;
-        n /= 10;
+class Solution {
+public:
+    bool isPalindrome(int x) {
+        if(x < 0)
+            return false;
+        long int num = x, reverse = 0, curr = 0;
+        while(x != 0){
+            curr = x%10;
+            reverse = reverse*10 + curr;
+            x /= 10;
+        }
+        if(num == reverse)
+            return true;
+        else{
+            return false;
+        }
     }
-    if(b == x){
-        cout << "True" << endl;
-    }else if(x < 0){
-        cout << "False" << endl;
-    }else if(x > 10){
-        cout << "True" << endl;
-    }else{
-        cout << "False" << endl;
-    }
-}
+};
